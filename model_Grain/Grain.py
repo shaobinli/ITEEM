9aa2668544a411ce1fb35038d9c3592f205c862a
Shaobin Li <leeshbin@gmail.com>
2021-07-20 20:53:22 -0500

    GP model updated

--- model_Grain/Grain.py
+++ model_Grain/Grain.py
@@ -24,7 +24,7 @@ depreciation_rate = depreciation_rate1 + depreciation_rate2
 class Grain(object):  
     def __init__(self, plant_type, plant_capacity, tech_GP):
         '''
-        plant_type (int) = {1:'corn_wet', 2:'corn_dry', 3:'soybean'}
+        plant_type (int) = {1:'corn_wet', 2:'corn_dry'}
         plant_capacity (metric ton corn/yr for corn_wet and million gallon ethanol/yr for corn_dry) (float) 
                         [0.67, 0.95, 1.7, 2.1, 5.0, 40, 80, 120, 240, 300]
         tech_GP (int) = {1: 'base case', 2: 'P recovery case'}
@@ -36,13 +36,12 @@ class Grain(object):
             self.plant_capacity = 8580*plant_capacity/(10**6) # convert MGD of ethanol to metric ton of corn
         self.tech_GP = tech_GP
         tech_GPs = {1: 'base case', 2: 'P recovery case'}      
-        plant_types = {1:'corn_wet', 2:'corn_dry', 3:'soybean'}
+        plant_types = {1:'corn_wet', 2:'corn_dry'}
         plant_capacities = [0.67, 0.95, 1.7, 2.1, 5.0, 40, 80, 120, 240, 300]
         if self.tech_GP not in tech_GPs.keys():
             raise ValueError("Invalid scenario")
         if self.plant_type not in plant_types.keys():
             raise ValueError("Invalid plant_type")
-
         if self.plant_type == 1:
             self.df = df_wetmilling
         elif self.plant_type ==2:
@@ -77,8 +76,8 @@ class Grain(object):
     def get_energy_use(self):
         '''return a list of energy_use, including natural gas and electricity in MJ/yr'''
         energy_use = []
-        op_ele_base = self.df[self.column][7]*self.plant_capacity*1000*3.6*10**6 # 1 kWh = 3.6 MJ
-        op_ng_base = self.df[self.column][8]*self.plant_capacity*1000*10**6
+        op_ele_base = self.df[self.column][7]*self.plant_capacity*3.6*10**6 # 1 kWh = 3.6 MJ
+        op_ng_base = self.df[self.column][8]*self.plant_capacity*10**6
         op_tot_base = op_ele_base + op_ng_base
         if self.tech_GP==1:
             energy_use = [op_tot_base]
@@ -122,8 +121,7 @@ class Grain(object):
         cost_facility_dependent = self.df[self.column][16]         # $/yr
         cost_chemical = self.df[self.column][17]*chem_index        # $/yr
         cost_op_base = cost_feedstock + cost_chemical + cost_utility + cost_facility_dependent # $/yr
-        cost_labor = self.df[self.column][13] - self.df[self.column][14] - self.df[self.column][15] \
-        - self.df[self.column][16] - self.df[self.column][17]  # $/yr
+        cost_labor = self.df[self.column][13] - self.df[self.column][14] - self.df[self.column][15] - self.df[self.column][16] - self.df[self.column][17]  # $/yr
         cost_op_base = cost_op_base + cost_labor
         
         if self.tech_GP==1:
@@ -139,11 +137,11 @@ class Grain(object):
             cost = [cost_op_base, cost_op_rP, cost_op_total]
         return cost
 
-    def get_revenue(self, starch_price=320, ethanol_price=1.32, rp_price=393.4, p_credit=0.0, grain_product_index = 1.0, rP_index=1.0):
+    def get_revenue(self, starch_price=320.36, ethanol_price=1.48, rp_price=351.05, p_credit=0.0, grain_product_index = 1.0, rP_index=1.0):
         '''
         return revenue gross revenue ($/yr)
-        starch_price = 320 $/ton; rP_price = 393.4 $/ton; CGF = 98.7 $/ton; CGM = 454.5 $/ton; Dried germ = 300 $/ton
-        DDGS_price = 152.8 $/ton; Corn oil = 750 $/ton; Ethanol = 1.32 $/gal; p_credit=5% price increase for byproduct
+        starch_price = 320.36 $/ton; rP_price = 351.05 $/ton; CGF = 98.7 $/ton; CGM = 454.5 $/ton; Dried germ = 300 $/ton
+        DDGS_price = 152.8 $/ton; Corn oil = 560 $/ton; Ethanol = 1.48 $/gal; p_credit=5% price increase for byproduct
         '''
         revenue = []
         plant_capacity = self.df[self.column][0]  # MT corn/yr
@@ -157,30 +155,29 @@ class Grain(object):
                 + self.df[self.column][26]*98.7*(1+p_credit) \
                 + self.df[self.column][27]*454.5 \
                 + self.df[self.column][28]*300)*grain_product_index \
-                + self.df[self.column][19]*rp_price*rP_index/1000 \
-                + plant_capacity*self.df[self.column][26]/2276*232*p_credit # 2726 ton of CGF or DDGS required for StoneRidge feedlot; 232kg P reduction/yr for StoneRidge
+                + self.df[self.column][19]*rp_price*rP_index/1000
+                # + plant_capacity*self.df[self.column][26]/2276*232*p_credit # 2726 ton of CGF or DDGS required for StoneRidge feedlot; 232kg P reduction/yr for StoneRidge
                 
                 p_credit2 = self.df[self.column][26]*98.7/1000*p_credit
                 revenue_rP = revenue_tot - revenue_base
                 revenue = [revenue_base, revenue_rP, p_credit2, revenue_tot]
         elif self.plant_type == 2:
             revenue_base = plant_capacity*(self.df[self.column][3]*ethanol_price + self.df[self.column][4]*152.8/1000 + 
-                                           self.df[self.column][5]/1000*750)*grain_product_index
+                                           self.df[self.column][5]/1000*560)*grain_product_index
             if self.tech_GP==1:
                 revenue = [revenue_base]
             elif self.tech_GP==2:
                 revenue_tot = plant_capacity*(self.df[self.column][25]*ethanol_price \
                 + self.df[self.column][26]*152.8/1000*(1+p_credit) \
-                + self.df[self.column][27]/1000*750)*grain_product_index \
-                + self.df[self.column][19]*rp_price*rP_index/1000 \
-                + plant_capacity*self.df[self.column][26]/2276/1000*232*p_credit
-                # 1029600*0.3/2276*232*20
+                + self.df[self.column][27]/1000*560)*grain_product_index \
+                + self.df[self.column][19]*rp_price*rP_index/1000
+
                 p_credit2 = self.df[self.column][26]*152.8/1000*p_credit
                 revenue_rP = revenue_tot - revenue_base
                 revenue = [revenue_base, revenue_rP, p_credit2, revenue_tot]       
         return revenue
     
-    def get_profit(self, r, starch_price=320, ethanol_price=1.32, rp_price=393.4, p_credit=0.0, grain_product_index=1.0, 
+    def get_profit(self, r, starch_price=320.36, ethanol_price=1.48, rp_price=351.05, p_credit=0.0, grain_product_index=1.0, 
                    rP_index=1.0, feedstock_index=1.0, chem_index=1.0, utility_index=1.0, tax=0.35):
         '''return net profit as annualized value, $/yr '''
         revenue = self.get_revenue(starch_price=starch_price, ethanol_price=ethanol_price, p_credit=p_credit, rp_price=rp_price, 
@@ -188,12 +185,15 @@ class Grain(object):
         cost = self.get_cost(feedstock_index=feedstock_index, chem_index=chem_index, utility_index=utility_index)[-1]
         taxable_income = revenue - cost
         net_profit = taxable_income*(1-tax)           # assume 40% as default tax
-        depreciation_cost = self.df[self.column][16]  # $/yr
-        cash_flow = net_profit + depreciation_cost    # $/yr
+
         if self.tech_GP==1:
             direct_fixed_cost = self.df[self.column][12]  # $/yr
+            depreciation_cost = self.df[self.column][16]  # $/yr
+            # depreciation_cost = direct_fixed_cost*0.0655 # $/yr
         elif self.tech_GP==2:
             direct_fixed_cost = self.df[self.column][12] + self.df[self.column][34]  # $/yr
+            depreciation_cost = self.df[self.column][16] + self.df[self.column][38]  # $/yr
+            # depreciation_cost = direct_fixed_cost*0.0655 # $/yr
         working_capital = direct_fixed_cost*0.05      # assume 5%
         cash_flow_list = [-(direct_fixed_cost*tax+working_capital), -(direct_fixed_cost*(1-tax))]
         for i in depreciation_rate:
@@ -203,27 +203,25 @@ class Grain(object):
         npv = npf.npv(r, cash_flow_list)
         irr = npf.irr(cash_flow_list)
         annualized_value = npv/annuity_factor(20, r)
-        
         a = [cost, revenue, taxable_income, net_profit]
-        
-        return a, irr, npv, annualized_value
+        b = [direct_fixed_cost, working_capital, depreciation_cost, cash_flow_list]
+        return a, b, irr, npv, annualized_value
 
 # wet_1 = Grain(plant_type=1, plant_capacity=5.0, tech_GP=2)
-# (Grain(plant_type=1, plant_capacity=2.1, tech_GP=1).get_energy_use()[-1] + \
-#     Grain(plant_type=1, plant_capacity=5.0, tech_GP=1).get_energy_use()[-1] + \
-#         Grain(plant_type=2, plant_capacity=120, tech_GP=1).get_energy_use()[-1])/(10**6)
+# (Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_energy_use()[-1] + \
+#     Grain(plant_type=1, plant_capacity=5.0, tech_GP=2).get_energy_use()[-1] + \
+#         Grain(plant_type=2, plant_capacity=120, tech_GP=2).get_energy_use()[-1])/(10**6)
 
 # Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_energy_use()
-# wet_1_base = Grain(plant_type=1, plant_capacity=2.1, tech_GP=1).get_profit(r=0.07, p_credit=0.0)[0]
-# wet_1_rp = Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_profit(r=0.07, p_credit=0.0)[-1]
-# wet_1_base - wet_1_rp
+# wet_1_base = Grain(plant_type=1, plant_capacity=2.1, tech_GP=1).get_profit(r=0.07, p_credit=0.0)
+# wet_1_rp = Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_profit(r=0.07, p_credit=0.0)
+# Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_revenue()
+# Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_cost()
+# cash_flow_list=Grain(plant_type=1, plant_capacity=2.1, tech_GP=2).get_profit(r=0.07, p_credit=0.0)[1][-1]
 
-# wet_2_base = Grain(plant_type=1, plant_capacity=5.0, tech_GP=1).get_profit(r=0.07, p_credit=0.0)[-1]
-# wet_2_rp = Grain(plant_type=1, plant_capacity=5.0, tech_GP=2).get_profit(r=0.07, p_credit=0.0)[-1]
-# wet_2_base - wet_2_rp
-
-# dry_1_base = Grain(plant_type=2, plant_capacity=120, tech_GP=1).get_profit(r=0.07, p_credit=0.0)[-1]
-# dry_1_rp = Grain(plant_type=2, plant_capacity=120, tech_GP=2).get_profit(r=0.07, p_credit=0.0)[-1]
+# dry_1_base = Grain(plant_type=2, plant_capacity=120, tech_GP=1).get_profit(r=0.07, p_credit=0.0)
+# dry_1_rp = Grain(plant_type=2, plant_capacity=120, tech_GP=2).get_profit(r=0.07, p_credit=0.0)
+# dry_1_rp_revenue = Grain(plant_type=2, plant_capacity=120, tech_GP=2).get_revenue()
 # dry_1_base - dry_1_rp
 
 # annuity_factor(20, 0.07)
